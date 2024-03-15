e92f253ee70e55db9fb0af5b945b862b061465a1
y0014984 <y0014984@gmail.com>
2024-03-15 10:48:25 +0100

    removed modded fall damage

--- scripts/Game/Components/Damage/GRAD_CharacterDamageManagerComponent.c
+++ scripts/Game/Components/Damage/GRAD_CharacterDamageManagerComponent.c
@@ -33,72 +33,72 @@ modded class SCR_CharacterDamageManagerComponent : SCR_DamageManagerComponent
 			}
 		}
 
-		// if damage type is 'collision' which could be for example fall damage
-		// the the damage is also applied on all 6 leg parts to create treatable injuries
-		
-		if (damageContext.damageType == EDamageType.COLLISION)
-		{
-			array<HitZone> hitZones = {};
-			GetAllHitZones(hitZones);
-			
-			foreach (HitZone hitZone : hitZones)
-			{
-				float fallDamage;
-				
-				switch (hitZone.GetName())
-				{
-					case "RThigh":
-						fallDamage = damageContext.damageValue * 0.1;
-						break;
-					case "LThigh":
-						fallDamage = damageContext.damageValue * 0.1;
-						break;
-					case "RCalf":
-						fallDamage = damageContext.damageValue * 0.3;
-						break;
-					case "LCalf":
-						fallDamage = damageContext.damageValue * 0.3;
-						break;
-					case "RFoot":
-						fallDamage = damageContext.damageValue * 0.1;
-						break;
-					case "LFoot":
-						fallDamage = damageContext.damageValue * 0.1;
-						break;
-					default:
-						break;
-				}
-				
-				float health = hitZone.GetHealth();
-				
-				float newHealth = health - fallDamage;
-				
-				if (newHealth <= 0)
-					hitZone.SetHealthScaled(0.01);
-				else
-					hitZone.SetHealth(newHealth);
-				
-				// if the collision damage is above threshold then
-				// the character gets unconscious for a certain amount of time
-				
-				// a damage of 25 is equal to a fall from a height of roughly 8-10 meters
-				if (damageContext.damageValue > 25)
-				{
-					ChimeraCharacter character = ChimeraCharacter.Cast(GetOwner());
-					if (!character)
-						return;
-					
-					CharacterControllerComponent controller = character.GetCharacterController();
-					if (!controller)
-						return;
-		
-					controller.SetUnconscious(true);
-					
-					// wake up after 15 sec
-					GetGame().GetCallqueue().CallLater(controller.SetUnconscious, 15000, false, false);
-				}
-			}
-		}
+//		// if damage type is 'collision' which could be for example fall damage
+//		// the the damage is also applied on all 6 leg parts to create treatable injuries
+//		
+//		if (damageContext.damageType == EDamageType.COLLISION)
+//		{
+//			array<HitZone> hitZones = {};
+//			GetAllHitZones(hitZones);
+//			
+//			foreach (HitZone hitZone : hitZones)
+//			{
+//				float fallDamage;
+//				
+//				switch (hitZone.GetName())
+//				{
+//					case "RThigh":
+//						fallDamage = damageContext.damageValue * 0.1;
+//						break;
+//					case "LThigh":
+//						fallDamage = damageContext.damageValue * 0.1;
+//						break;
+//					case "RCalf":
+//						fallDamage = damageContext.damageValue * 0.3;
+//						break;
+//					case "LCalf":
+//						fallDamage = damageContext.damageValue * 0.3;
+//						break;
+//					case "RFoot":
+//						fallDamage = damageContext.damageValue * 0.1;
+//						break;
+//					case "LFoot":
+//						fallDamage = damageContext.damageValue * 0.1;
+//						break;
+//					default:
+//						break;
+//				}
+//				
+//				float health = hitZone.GetHealth();
+//				
+//				float newHealth = health - fallDamage;
+//				
+//				if (newHealth <= 0)
+//					hitZone.SetHealthScaled(0.01);
+//				else
+//					hitZone.SetHealth(newHealth);
+//				
+//				// if the collision damage is above threshold then
+//				// the character gets unconscious for a certain amount of time
+//				
+//				// a damage of 25 is equal to a fall from a height of roughly 8-10 meters
+//				if (damageContext.damageValue > 25)
+//				{
+//					ChimeraCharacter character = ChimeraCharacter.Cast(GetOwner());
+//					if (!character)
+//						return;
+//					
+//					CharacterControllerComponent controller = character.GetCharacterController();
+//					if (!controller)
+//						return;
+//		
+//					controller.SetUnconscious(true);
+//					
+//					// wake up after 15 sec
+//					GetGame().GetCallqueue().CallLater(controller.SetUnconscious, 15000, false, false);
+//				}
+//			}
+//		}
 		
 		super.OnDamage(damageContext);
 	}
